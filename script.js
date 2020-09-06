65ec1d5e8a7dfb3a7b4dbcfec0fdeeab3d3b350f
Vikas Choubey <vikas.cby@gmail.com>
2020-09-06 16:58:11 +0530

    add support for twopart joke

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const jokeCategory = document.querySelector(".joke-category");
 const refreshBtn = document.querySelector(".button-primary");
 const footer = document.querySelector("footer");
 
-const url = 'https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw&type=single';
+const url = 'https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw';
 
 const showLoading = () => {
     jokeCategory.textContent = "Loading...";
     jokeContainer.classList.add("hidden");
 }
 
-const showJoke = ({ category, joke }) => {
+const showJoke = (category, joke) => {
     jokeCategory.textContent = `Category : ${category}`;
     jokeContainer.classList.remove("hidden");
-    jokeContainer.textContent = joke;
+
+    if (joke.length == 1)
+        jokeContainer.textContent = joke;
+    else
+        jokeContainer.innerText = `${joke[0]}
+        ${joke[1]}`;
 }
 const getNewJoke = async () => {
     showLoading();
     const response = await fetch(url).then(res => res.json());
-    showJoke(response);
+    response.hasOwnProperty('joke') ? showJoke(response.category, [response.joke]) : showJoke(response.category, [response.setup, response.delivery]);
 }
 
 window.addEventListener('load', getNewJoke);
